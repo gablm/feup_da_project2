59dfa01d01bf0aa13e614917ba90a7a9b8f3c6f6
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-05-18 00:37:42 +0100

    Update realworld.cpp

--- src/manager/heuristics/realworld.cpp
+++ src/manager/heuristics/realworld.cpp
@@ -1,7 +1,25 @@
 #include "../manager.h"
+#include "../../ui/ui.h"
+#include <stack>
 
 ReturnDataTSP Manager::realWorldHeuristic(Vertex *base)
 {
-	(void)base;
-	return {-1, {}, {}, -1};
+	auto start = std::chrono::high_resolution_clock::now();
+	double totalDistance = 0;
+	std::vector<int> stops;
+	std::vector<double> distances;
+
+	for (auto vtx : network.getVertexSet()) {
+		if (vtx->getAdj().size() < 2)
+		{
+			UI::showMessage(
+				"INVALID GRAPH",
+				"At least one or more nodes only have one or less edges. \nThis means a path visiting every nodes only once is impossible to obtain."
+			);
+			return {-1, {}, {}, -1};
+		}
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	return {std::chrono::duration<double>(end - start).count(), stops, distances, totalDistance};
 }
